dd389cd3f67e77bdf8ccb171569f4c8d25c814ac
Azazel 00 <pfbhjdfhn@mail.ru>
2023-10-15 22:27:16 +0500

    Task6

---
+++ Tyuiu.ZairovAR.Sprint2.Task6.V4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZairovAR.Sprint2.Task6.V4.Lib;
+namespace Tyuiu.ZairovAR.Sprint2.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Заиров А. Р. | ACOиУБ 23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Заиров Артур Ранисович | AСОиУБ 23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Написать программу, которая определяет название                         *");
+            Console.WriteLine("* соответствующей масти, принимая от пользователя цифры.                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Введите цифру (от 1 до 4 включительно):");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" Ваша масть: {ds.FindCardSuit(n)}");
+            Console.ReadKey();
+        }
+    }
+}
